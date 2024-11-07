bc35c01650bb31cbc0d1c83954e6ea52be855d47
Mamadou Lamine Diamban <mlamine.diamban@gmail.com>
2024-11-07 22:26:28 +0100

    test pendu

--- src/pendu.py
+++ src/pendu.py
@@ -4,31 +4,29 @@ import random
 
 dictionary = pd.read_table("./data/mots.txt", header=0, names=['words'])
 
-
-
 def word_to_guess(random_word):
     return len(random_word) * "_"
 
-word = word_to_guess(random_word_to_guess)
 
 def replace_letter(letter, random_word, word):
         return ''.join([letter if random_word[i] == letter else word[i] for i in range(len(random_word))])
 
 
-def game(random_word_to_guess, word):
+def game():
     random_word_to_guess = random.choice(dictionary['words'])
+    print(random_word_to_guess)
     number_attempts = 8
     word = word_to_guess(random_word_to_guess)
     while number_attempts > 0:
         print(word)
         letter = input("Enter a letter: ")[0].lower()
-        if(letter in random_word_to_guess):
+        if letter in random_word_to_guess:
             word = replace_letter(letter, random_word_to_guess, word)
             if word == random_word_to_guess:
                 print("Congratulations, you've guessed the word!")
                 response = input("Would do you try again ? (yes/no)")
                 if response in ('y', 'yes'):
-                    game(random_word_to_guess, word)
+                    game()
                 else:
                     break
         else:
@@ -40,9 +38,9 @@ def game(random_word_to_guess, word):
         
         response = input("Would do you try again ? (yes/no)")
         if response in ('y', 'yes'):
-            game(random_word_to_guess, word)
+            game()
 
-game(random_word_to_guess, word)
+game()
 
 
     
